f283432fc19bbedcfb932f33be2b8aed09a2fe55
aakshisinha <aakshi@bu.edu>
2025-07-01 06:57:39 -0400

    first working prototype with basic randomizer function

---
+++ index.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", () => {
+  const values = [
+    "Chicken Parm",
+    "Fried Rice",
+    "Avo Toast",
+    "Pasta & Beef",
+    "Cereal Milk",
+    "Yogurt & Granola",
+    "Brocoli Soup",
+    "Egg Drop Soup",
+    "Ramen",
+    "Hot Chocolate",
+    "Hot milk",
+    "Tea",
+    "read?",
+  ];
+
+  const symbols = ["♠", "♣", "♦", "♥"];
+  const colors = ["red", "black"];
+
+  const cardSymbolHeader = document.getElementById("card-symbol-header");
+  const cardValue = document.getElementById("card-value");
+  const cardSymbolFooter = document.getElementById("card-symbol-footer");
+
+  const randomValueIndex = Math.floor(Math.random() * values.length);
+  const randomSymbolIndex = Math.floor(Math.random() * symbols.length);
+  const randomColorIndex = Math.floor(Math.random() * colors.length);
+
+  cardSymbolHeader.innerHTML = `<p style="color:${colors[randomColorIndex]}">${symbols[randomSymbolIndex]}</p>`;
+  cardValue.innerHTML = `<p>${values[randomValueIndex]}</p>`;
+  cardSymbolFooter.innerHTML = `<p style="color:${colors[randomColorIndex]}">${symbols[randomSymbolIndex]}</p>`;
+});
